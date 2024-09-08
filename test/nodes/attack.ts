f5d8ea0dd706d9d32babeed3f94cd5ca338dff32
codetypes <codetypes@gmail.com>
2024-09-08 10:53:13 +0800

    node arg support array type

--- test/nodes/attack.ts
+++ test/nodes/attack.ts
@@ -16,6 +16,59 @@ export class Attack extends Process {
     }
 
     override get descriptor(): NodeDef {
-        return { name: "Attack", type: "Action", desc: "攻击", input: ["目标敌人"] };
+        return {
+            name: "Attack",
+            type: "Action",
+            desc: "攻击",
+            input: ["目标敌人", "技能..."],
+            output: ["结果", "数值..."],
+            args: [
+                {
+                    name: "distance",
+                    type: "int?",
+                    desc: "攻击距离",
+                },
+                {
+                    name: "range",
+                    type: "int",
+                    desc: "距离",
+                },
+                {
+                    name: "enemy",
+                    type: "string[]",
+                    desc: "敌人",
+                },
+                {
+                    name: "multi",
+                    type: "boolean[]",
+                    desc: "是否多目标",
+                },
+                {
+                    name: "data",
+                    type: "json[]",
+                    desc: "数据",
+                },
+                {
+                    name: "velocity",
+                    type: "float[]",
+                    desc: "速度",
+                },
+                {
+                    name: "target",
+                    type: "enum[]",
+                    desc: "目标",
+                    options: [
+                        {
+                            name: "敌人",
+                            value: "enemy",
+                        },
+                        {
+                            name: "自己",
+                            value: "self",
+                        },
+                    ],
+                },
+            ],
+        };
     }
 }
